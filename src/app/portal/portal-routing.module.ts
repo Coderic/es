7b16059b41e45ef0610406d521b421684ac3c6b7
Neftali Yagua <neftaliyagua@fedoraproject.org>
2023-12-24 02:44:16 -0500

    Basic

--- src/app/portal/portal-routing.module.ts
+++ src/app/portal/portal-routing.module.ts
@@ -4,25 +4,43 @@ import { PortalComponent } from './portal.component';
 import { SecurityGuard } from '../security/security.guard';
 import { PrepararComponent } from '../preparar/preparar.component';
 import { HomeComponent } from './home/home.component';
-
+import { AboutComponent } from './about/about.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { RefundsComponent } from './refunds/refunds.component';
+import { LegalComponent } from './legal/legal.component';
+import { TosComponent } from './tos/tos.component';
+import { FaqsComponent } from './faqs/faqs.component';
 
 const routes: Routes = [
   {
-    path: '', component: PortalComponent,
+    path: '',
+    component: PortalComponent,
     children: [
       { path: '', component: HomeComponent },
-      { path: 'preparar/:id/:hash', component: PrepararComponent }
-    ]
+      { path: 'about', component: AboutComponent },
+      { path: 'privacy', component: PrivacyComponent },
+      { path: 'delivery', component: DeliveryComponent },
+      { path: 'refunds', component: RefundsComponent },
+      { path: 'legal', component: LegalComponent },
+      { path: 'tos', component: TosComponent },
+      { path: 'faqs', component: FaqsComponent },
+      { path: 'preparar/:id/:hash', component: PrepararComponent },
+    ],
   },
   {
     path: 'development',
     loadChildren: () =>
-      import('./development/development.module').then((m) => m.DevelopmentModule),
+      import('./development/development.module').then(
+        (m) => m.DevelopmentModule
+      ),
   },
   {
     path: 'crowdfunding',
     loadChildren: () =>
-      import('./crowdfunding/crowdfunding.module').then((m) => m.CrowdfundingModule),
+      import('./crowdfunding/crowdfunding.module').then(
+        (m) => m.CrowdfundingModule
+      ),
   },
   {
     path: 'coworking',
@@ -32,7 +50,9 @@ const routes: Routes = [
   {
     path: 'freelancers',
     loadChildren: () =>
-      import('./freelancers/freelancers.module').then((m) => m.FreelancersModule),
+      import('./freelancers/freelancers.module').then(
+        (m) => m.FreelancersModule
+      ),
   },
   {
     path: 'learning',
@@ -43,10 +63,10 @@ const routes: Routes = [
     path: 'community',
     loadChildren: () =>
       import('./community/community.module').then((m) => m.CommunityModule),
-  }
+  },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PortalRoutingModule { }
+export class PortalRoutingModule {}
--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -4,12 +4,26 @@ import { CommonModule } from '@angular/common';
 import { PortalRoutingModule } from './portal-routing.module';
 import { PortalComponent } from './portal.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { RefundsComponent } from './refunds/refunds.component';
+import { LegalComponent } from './legal/legal.component';
+import { TosComponent } from './tos/tos.component';
+import { FaqsComponent } from './faqs/faqs.component';
 
 
 @NgModule({
   declarations: [
     PortalComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PrivacyComponent,
+    DeliveryComponent,
+    RefundsComponent,
+    LegalComponent,
+    TosComponent,
+    FaqsComponent
   ],
   imports: [
     CommonModule,
