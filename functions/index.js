c9ef69e303e7690aeb57a041340459f51ef85c64
Neftalí Yagua <neftaliyagua@fedoraproject.org>
2024-06-15 04:05:21 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,3 @@
-addEventListener("fetch", (event) => {
-  return event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
-});
-
-async function handleRequest(request) {
-  var userLang = Intl.DateTimeFormat().resolvedOptions().locale;
-  
-  var urls = {
-    'es': 'https://es.code-ric.com/'
-  };
-  var defaultUrl = 'https://en.code-ric.com/';
-
-  var url = urls[userLang.substring(0, 2)] ?? defaultUrl;
+export function onRequest(context) {
   return new Response("Hello, world!")
-  return await new Response(userLang.substring(0, 2), {
-    headers: {
-      "Location": url,
-    },
-  })
 }
