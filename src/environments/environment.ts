1c1467912334073adb431a9005936e0983e47cc6
Neftalí Yagua <neftaliyagua@fedoraproject.org>
2025-01-12 02:40:57 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   api: '/api/',
   clientId: 'In43D8hfptI5B17Xo7XZX4aBkhfMuH56',
-  authority: 'https://auth.coderic.org'
+  authority: 'https://coderic.eu.auth0.com'
 };
