8c47bf94414235cb5f1800e2d1e8175a2f084940
Neftali Yagua <neftaliyagua@fedoraproject.org>
2025-01-13 00:49:49 -0500

    Edit profile

--- src/app/account/navigation/navigation.component.ts
+++ src/app/account/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
     selector: 'app-navigation',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class NavigationComponent {
-
+  constructor(public auth: AuthService) {}
 }
--- src/app/account/profile/profile.component.ts
+++ src/app/account/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
     selector: 'app-profile',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class ProfileComponent {
+  constructor(public auth: AuthService) {}
 
 }
