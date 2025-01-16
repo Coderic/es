76e544bdfe951bb79f0a03a484e1a8f1ad861cd3
Neftalí Yagua <neftaliyagua@fedoraproject.org>
2025-01-16 08:36:03 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 const { domain, clientId, audience, serverUrl } = {
   "domain": "auth.coderic.org",
   "clientId": "In43D8hfptI5B17Xo7XZX4aBkhfMuH56",
-  "audience": "https://coderic.org",
+  "audience": "https://api.coderic.net",
   "serverUrl": "http://localhost:6060"
 };
 export const environment = {
